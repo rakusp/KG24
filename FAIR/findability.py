42a43a2091c1800b4b6d7112548d5c85bd1d335a
mtomczyk <mtomczyk@match-trade.com>
2024-11-20 11:19:48 +0100

    add early interoperability properties

--- FAIR/findability.py
+++ FAIR/findability.py
@@ -18,7 +18,8 @@ def findability_properties(connection: SPARQLWrapper, datasets_query: str, prefi
     """
     Finds properties of Findability principle of FAIR data - currently the dataset's publisher's name
     (important note: it needs to be provided in the input query), its title in English (if present),
-    its number of keywords, its date of being issued (if present), and itstype (if present).
+    its number of keywords, its date of being issued (if present), its type (if present)
+    and the location associated with it (if present).
         :param connection: SPRAQLWrapper connection
         :param datasets_query: SPARQL query to get the datasets
         :param prefixes: prefixes to be used in the SPARQL query
@@ -27,7 +28,7 @@ def findability_properties(connection: SPARQLWrapper, datasets_query: str, prefi
     query = """
         SELECT DISTINCT ?datasetURI ?publisher
         (IF(LANG(?datasetTitle) = "" || LANG(?datasetTitle) = "en", ?datasetTitle, "") as ?englishTitle)
-         (COUNT(?keyword) AS ?keywords) ?issuedDate ?datasetType
+         (COUNT(?keyword) AS ?keywords) ?issuedDate ?datasetType ?datasetLocation
          WHERE
          {
             {   
@@ -37,6 +38,7 @@ def findability_properties(connection: SPARQLWrapper, datasets_query: str, prefi
             OPTIONAL { ?datasetURI dcat:keyword ?keyword. }
             OPTIONAL { ?datasetURI dct:issued ?issuedDate. }
             OPTIONAL {?datasetURI dct:type ?datasetType. }
+            OPTIONAL {?datasetURI dct:spatial ?datasetLocation. }
          }
     """
     return utils.SPARQL_to_df(connection, query, prefixes)
--- FAIR/interoperability.py
+++ FAIR/interoperability.py
@@ -4,9 +4,39 @@ FAIR.interoperability
 
 This module implements the assesments of Interoperability principle of FAIR data.
 """
-
 import sys
+
+import pandas as pd
+from SPARQLWrapper import SPARQLWrapper
+
 import utils
 sys.path.append('../project')
 
-# todo Michał
+
+def interoperability_properties(connection: SPARQLWrapper, datasets_query: str, prefixes: str = None) -> pd.DataFrame:
+    """
+    Finds properties of Interoperability principle of FAIR data - currently the number of resources
+    (publications, etc.) pointing to it, its language (if present), access rights associated with it
+    and the dataset for which it is a version, edition or an adaptation of (if present).
+        :param connection: SPRAQLWrapper connection
+        :param datasets_query: SPARQL query to get the datasets
+        :param prefixes: prefixes to be used in the SPARQL query
+        :return: pd.DataFrame with the dataset URIs and the values of the properties
+    """
+    query = """
+        SELECT DISTINCT ?datasetURI 
+        (COUNT(?isReferencedBy) as ?referencesNumber) 
+        ?datasetLanguage ?accessRights ?isVersionOf
+        
+         WHERE
+         {
+            {   
+            """ + datasets_query + """
+            }
+            OPTIONAL {?datasetURI dct:isReferencedBy ?isReferencedBy. }
+            OPTIONAL {?datasetURI dct:language ?datasetLanguage. }
+            OPTIONAL {?datasetURI dct:accessRights ?accessRights. }
+            OPTIONAL {?datasetURI dct:isVersionOf ?isVersionOf. }
+         }
+    """
+    return utils.SPARQL_to_df(connection, query, prefixes)
